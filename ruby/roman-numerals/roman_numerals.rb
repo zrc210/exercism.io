e623581ecfc2c42077e63cd58236c512bab203e0
zrc210 <zubair@chaudary.com>
2016-12-09 18:53:36 -0500

    rework roman_numerals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -11,21 +11,18 @@ end
 module RomanNumerals
   ROMAN_NUMERALS = {
     1000 => 'M',
+    900 => 'CM',
     500 => 'D',
+    400 => 'CD',
     100 => 'C',
+    90 => 'XC',
     50 => 'L',
+    40 => 'XL',
     10 => 'X',
+    9 => 'IX',
     5 => 'V',
+    4 => 'IV',
     1 => 'I'
-  }.freeze
-
-  LONG_TO_SHORT = {
-    'DCCCC' => 'CM',
-    'CCCC'  => 'CD',
-    'LXXXX' => 'XC',
-    'XXXX'  => 'XL',
-    'VIIII' => 'IX',
-    'IIII'  => 'IV'
   }.freeze
 
   class << self
@@ -37,15 +34,7 @@ module RomanNumerals
         result << roman_letter * needed
         remainder
       end
-      convert(result)
-    end
-
-    private
-    def convert(roman_str)
-      LONG_TO_SHORT.reduce(roman_str) do |converted, mapping|
-        long, short = mapping
-        converted.gsub(/#{long}/, short)
-      end
+      result
     end
   end
 end
