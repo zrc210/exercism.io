f478f2fcb62f0d50de75bd170904caf1950eaff0
zrc210 <zubair@chaudary.com>
2016-12-09 22:21:34 -0500

    reworked

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -2,30 +2,71 @@ module BookKeeping
   VERSION = 2
 end
 
-class NameGenerator
+class Database
   def initialize
-    srand
+    @@storage ||= {}
   end
+
+  def add(name)
+    storage[name] = true
+    nil
+  end
+
+  def remove(name)
+    storage.delete(name)
+    nil
+  end
+
+  def exists?(name)
+    storage.key?(name)
+  end
+
+  private
+
+  def storage
+    @@storage
+  end
+end
+
+class NameGenerator
+  def initialize(randomizer: Random.new, database: Database.new)
+    @randomizer = randomizer
+    @database = database
+  end
+
+  def generate(old_name = nil)
+    database.remove(old_name) unless old_name
+    generated = name
+    generated = name while database.exists?(generated)
+    database.add(generated)
+    generated
+  end
+
+  private
 
   def name
     prefix + digits
   end
 
-  private
+  attr_reader :randomizer, :database
 
   ALPHABET = [*'A'..'Z'].freeze
 
   def prefix
-    2.times.reduce('') { |prefix| prefix << ALPHABET[rand(26)] }
+    2.times.reduce('') { |prefix| prefix << ALPHABET[random(26)] }
   end
 
   def digits
-    3.times.reduce('') { |digits| digits << rand(10).to_s }
+    format('%03i', random(1000))
+  end
+
+  def random(max)
+    randomizer.rand(max)
   end
 end
 
 class Robot
-  def initialize(name_generator = NameGenerator.new)
+  def initialize(name_generator: NameGenerator.new)
     @name_generator = name_generator
   end
 
@@ -34,7 +75,7 @@ class Robot
   end
 
   def name
-    @name ||= name_generator.name
+    @name ||= name_generator.generate(@name)
   end
 
   private
