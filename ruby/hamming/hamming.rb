d373074a5c14e829e6629668b16c08fa2b794b96
Zubair Chaudary <zubair@simulmedia.com>
2016-12-12 12:47:19 -0500

    refine hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,12 +3,16 @@ module BookKeeping
 end
 
 module Hamming
+  class StrandLengthError < ArgumentError
+    def initialize(message = 'Strands must be of equal length')
+      super
+    end
+  end
+
   class << self
-    def compute(a,b)
-      raise ArgumentError if a.length != b.length
-      [*0...a.length]
-        .map { |i| a[i] == b[i] ? 0 : 1}
-        .reduce(0, :+)
+    def compute(strand_1, strand_2)
+      raise StrandLengthError if strand_1.length != strand_2.length
+      strand_1.chars.zip(strand_2.chars).count{ |pairs| pairs[0] != pairs[1] }
     end
   end
 end
