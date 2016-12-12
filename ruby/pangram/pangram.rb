3b927e27615813199fe673a291efc763b2cef2da
Zubair Chaudary <zubair@simulmedia.com>
2016-12-12 13:15:37 -0500

    rework pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,9 +2,18 @@ module BookKeeping
   VERSION = 3
 end
 module Pangram
+  ENGLISH_ALPHABET = [*'a'..'z'].freeze
   class << self
-    def pangram?(str)
-      str.downcase.delete('^a-z').chars.uniq.length == 26
+    def pangram?(string, alphabet = ENGLISH_ALPHABET)
+      normalized = normalize(string, alphabet)
+      normalized.chars.uniq.length == alphabet.length
+    end
+
+    private
+
+    def normalize(string, alphabet)
+      delete_range = format('^%s-%s', *alphabet.minmax)
+      string.downcase.delete(delete_range)
     end
   end
 end
