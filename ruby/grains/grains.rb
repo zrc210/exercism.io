09cbe477671b9a45c8924710fa0d2aad1e72c325
Zubair Chaudary <zubair@simulmedia.com>
2016-12-13 19:52:52 -0500

    make generic

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,27 +1,45 @@
-class Grains
+module Grains
   def self.square(index)
-    new.square(index)
+    GrainBoard.new.square(index)
   end
 
   def self.total
-    new.total
+    GrainBoard.new.total
   end
 
-  def initialize(max_squares: 64)
-    @max_squares = 64
+  class GrainBoard
+    def initialize(base: 2, max_squares: 64)
+      raise(ArgumentError,
+            "base must be greater than 1") unless base > 1
+      raise(ArgumentError,
+           "max_squares must be a positive integer") unless max_squares > 0
+      @base = base
+      @max_squares = max_squares
+    end
+
+    def square(requested_square)
+      # the squares are indexed with a starting index of 1
+      index = requested_square - 1
+      raise(ArgumentError,
+            "Square must be in [1,#{max_squares}]") unless in_bounds?(index)
+      compute(index)
+    end
+
+    def total
+      # the sum(n=0, N) b**n = (((b**(N+1)) - 1)/(b-1))
+      (compute(max_squares) - 1) / (base - 1)
+    end
+
+    private
+
+    attr_reader :max_squares, :base
+
+    def compute(index)
+      base**(index)
+    end
+
+    def in_bounds?(index)
+      (0...max_squares).cover?(index)
+    end
   end
-
-  def square(index)
-    # the squares are indexed with a starting index of 1
-    2**(index - 1)
-  end
-
-  def total
-    # the sum(n=0, N) 2**n = (2**(N+1)) - 1
-    square(max_squares + 1) - 1
-  end
-
-  private
-
-  attr_reader :max_squares
 end
