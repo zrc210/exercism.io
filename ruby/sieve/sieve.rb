cb95da0f64c9ea00686469e724a5e389f72f98ff
zrc210 <zubair@chaudary.com>
2016-12-24 17:05:09 -0500

    new sieve method

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,21 +4,26 @@ end
 
 class Sieve
   def initialize(number)
-    @number_range = [*2..number].freeze
+    @number_range = (2..number).freeze
   end
 
   def primes
-    range = number_range.dup
-    range.each do |factor|
-      range.reject! { |number| factor?(number, factor) }
-    end
-    range
+    @primes ||= compute_primes
   end
 
   private
+
   attr_reader :number_range
 
-  def factor?(number, factor)
-    number > factor && number % factor == 0
+  def compute_primes
+    primes = number_range.to_a
+    primes.each_with_index do |factor, index|
+      next unless factor
+      start = index + factor
+      (start..primes.length).step(factor).each do |mark_index|
+        primes[mark_index] = nil
+      end
+    end
+    primes.compact
   end
 end
